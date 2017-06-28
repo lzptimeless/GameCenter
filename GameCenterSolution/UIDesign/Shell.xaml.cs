942409952981d52c6c0ada0f600a632a4d8a79f4
LingZhongping <zhongping.ling@razerzone.com>
2017-06-28 18:31:58 +0800

    更新

--- GameCenterSolution/UIDesign/Shell.xaml.cs
+++ GameCenterSolution/UIDesign/Shell.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace UIDesign
 {
@@ -22,7 +24,6 @@ namespace UIDesign
         public Shell()
         {
             InitializeComponent();
-            
         }
     }
 }
